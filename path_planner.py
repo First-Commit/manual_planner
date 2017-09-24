92475b8c38ed23d58902e7a8480e7a006bdb0852
Nic <nic.r.fischer@gmail.com>
2017-09-24 14:51:44 -0700

    Manual planning and viz

---
+++ path_planner.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import csv
+import math
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+kGatesFile = 'gates.csv'
+kPathInFile = 'path.csv'
+kPathOutFile = 'path_out.csv'
+kGateWidth = 1.3 # meters
+kGateThickness = 0.2 # meters
+kOriginShift = (3.0,0)
+
+def main():
+  fig1 = plt.figure()
+  ax1 = fig1.add_subplot(111, aspect='equal')
+  
+  gates_data = loadGates(kGatesFile)
+  drawGates(ax1, gates_data)
+  path_data = calcPath(gates_data)
+  drawPath(ax1, path_data)
+  savePath(path_data)
+  
+  plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
+  plt.axis('equal')
+  plt.axis('off')
+
+  plt.show()
+
+def savePath(path_data):
+  print("Wrote path to csv: {}".format(kPathOutFile))
+  with open(kPathOutFile, "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(path_data)
+
+def drawOrientation(ax1, center, angle):
+  delta_x = math.cos(angle*math.pi/180.0)*0.5
+  delta_y = math.sin(angle*math.pi/180.0)*0.5
+  ax1.add_patch(
+    patches.Arrow(
+      center[0],
+      center[1],
+      delta_x,
+      delta_y,
+      facecolor="#ff6600",
+      width=0.5
+    )
+  )
+
+def calcPath(gates_data):
+  waypoints_out = []
+  for i,row in enumerate(gates_data):
+    origin = (float(row['x'])+kOriginShift[0], float(row['y'])+kOriginShift[1])
+    shift = float(row['offset_y'])
+    angle = float(row['rotation'])
+    altitude = float(row['z'])
+    gate_center = [origin[0], origin[1]+shift, altitude, angle]
+    waypoint_before = float(row['waypoint_before'])
+    waypoint_after = float(row['waypoint_after'])
+    sign_flip = 1
+    y_135_fix = 0
+    if angle == -90 or angle == 90:
+      sign_flip = -1
+    if angle == -135:
+      y_135_fix = kGateWidth
+    if not waypoint_before == 0:
+      before_wp = [-math.cos(angle*math.pi/180.0)*waypoint_before + gate_center[0], sign_flip*math.sin(angle*math.pi/180.0)*waypoint_before + gate_center[1] + y_135_fix, altitude, angle]
+      waypoints_out.append(before_wp)
+    waypoints_out.append(gate_center)
+    if not waypoint_after == 0:
+      after_wp = [math.cos(angle*math.pi/180.0)*waypoint_after + gate_center[0], -sign_flip*math.sin(angle*math.pi/180.0)*waypoint_after + gate_center[1] + y_135_fix, altitude, angle]
+      waypoints_out.append(after_wp)
+  return waypoints_out
+
+def drawPath(ax1, path_data):
+  for i,wp in enumerate(path_data):
+    ax1.add_patch(
+      patches.Circle(
+        wp,           # (x,y)
+        0.2,          # radius
+        facecolor="#ff6600"
+      )
+    )
+    if i < len(path_data)-1:
+      delta = (path_data[i+1][0]-path_data[i][0], path_data[i+1][1]-path_data[i][1])
+      print("x1 {} x2 {} | y1 {} y2 {}".format(path_data[i+1][0], path_data[i][0], path_data[i+1][1], path_data[i][1]))
+      ax1.add_patch(
+        patches.Arrow(
+          wp[0],
+          wp[1],
+          delta[0],
+          delta[1]
+        )
+      )
+    drawOrientation(ax1, (wp[0],wp[1]), wp[3])
+
+
+def loadGates(gates_file):
+  print("Opening gates file: {}".format(gates_file))
+  reader = csv.DictReader(open(gates_file), delimiter=',', skipinitialspace=True)
+  return list(reader)
+
+def drawGates(ax1, gates_data):
+    for i,row in enumerate(gates_data):
+      origin = (float(row['x'])+kOriginShift[0], float(row['y'])+kOriginShift[1])
+      angle = float(row['rotation'])
+      shift = float(row['offset_y'])
+      # angle = 0  
+      bottomLeftOfRect = (origin[0]-kGateThickness/2.0, origin[1]-kGateWidth/2.0+shift)
+      if angle == -90:
+        bottomLeftOfRect = (origin[0]-kGateWidth/2.0, origin[1]+kGateThickness/2.0)
+      if angle == 90:
+        bottomLeftOfRect = (origin[0]+kGateWidth/2.0, origin[1]-kGateThickness/2.0)
+      if angle == -135:
+        # Hardcoded because geometry is hard
+        bottomLeftOfRect = (origin[0]-kGateWidth*0.30, origin[1]+kGateWidth*0.40)
+      if angle == 180:
+        bottomLeftOfRect = (origin[0]+kGateThickness/2.0, origin[1]+kGateWidth/2.0+shift)
+      ax1.add_patch(
+        patches.Rectangle(
+          bottomLeftOfRect,         # (x,y)
+          0.2,                      # width
+          kGateWidth,               # length
+          angle=angle
+        )
+      )
+      ax1.add_patch(
+        patches.Circle(
+          origin,   # (x,y)
+          0.2,          # radius
+          facecolor="#ff0000"
+        )
+      )
+      text_origin = (origin[0], origin[1]-2.0)
+      ax1.text(text_origin[0], text_origin[1], str(i+1))
+
+    # add coordinate system
+    ax1.add_patch(
+      patches.Arrow(
+        0.0,            # x
+        0.0,            # y
+        1.0,            # dx
+        0.0,            # dy
+        width=1.0,
+        facecolor="#00ff00"
+      )
+    )
+    ax1.text(1.1, -0.2, 'x')
+
+if __name__ == "__main__":
+  main()
